425b95a6193209845283cb862c32c3420f1b5e35
JanisataMJ <namuy.2547@gmail.com>
2024-09-08 03:38:49 +0700

    save

--- frontend/src/pages/list/index.tsx
+++ frontend/src/pages/list/index.tsx
@@ -23,79 +23,104 @@ import React from 'react';
 import { Table } from 'antd';
 import type { TableProps } from 'antd';
 import { Col, Row,Divider} from "antd";
+import "./list.css";
 
-interface DataType {
-  key: string;
-  name: string;
-  sid: string;
-  major: string;
-  year: number;
-  roomRate: string;
+
+const Listpages: React.FC = () => {
+  interface DataType {
+    key: string;
+    name: string;
+    sid: string;
+    major: string;
+    year: number;
+    roomRate: string;
+  }
+  
+  const columns: TableProps<DataType>['columns'] = [
+    
+    {
+      title: '',
+      dataIndex: 'key',
+      key: 'key',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'รหัสนักศึกษา',
+      dataIndex: 'sid',
+      key: 'sid',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'ชื่อ - นามสกุล',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'สำนัก',
+      dataIndex: 'major',
+      key: 'major',
+    },
+    {
+      title: 'ชั้นปี',
+      dataIndex: 'year',
+      key: 'year',
+    },
+    {
+      title: 'ค่าห้อง',
+      dataIndex: 'roomRate',
+      key: 'year',
+    },
+  ];
+  
+  const data: DataType[] = [
+    {
+      key: 'A',
+      name: 'John Brown',
+      sid: 'B6512345',
+      major: 'Engineering',
+      year: 3,
+      roomRate: '2,900',
+    },
+    {
+      key: 'B',
+      name: 'Jim Green',
+      sid: 'B6554321',
+      major: 'Engineering',
+      year: 3,
+      roomRate: '2,900',
+    },
+    {
+      key: 'C',
+      name: 'Joe Black',
+      sid: 'B6543210',
+      major: 'Engineering',
+      year: 3,
+      roomRate: '2,900',
+    },
+  ];
+  
+  
+  return (
+    <>
+      <br />
+      <div className="flex">
+        <div className='text-topic'>รายชื่อผู้พักร่วม</div>
+        <div className="text-right">Non-Air Conditioner</div>
+      </div>
+      <br />
+      <Divider />
+      <div className="flex">
+        <div className='box'>ห้อง 4100</div>
+        <div className='text-sub'>ปีการศึกษา 1/2565</div>
+      </div>
+      <Divider />
+      <div className='text-container'></div>  
+        <Table columns={columns} dataSource={data} pagination={false} />
+      <br/>
+    </>
+  );
+  
 }
 
-const columns: TableProps<DataType>['columns'] = [
-  
-  {
-    title: '',
-    dataIndex: 'key',
-    key: 'key',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'รหัสนักศึกษา',
-    dataIndex: 'sid',
-    key: 'sid',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'ชื่อ - นามสกุล',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'สำนัก',
-    dataIndex: 'major',
-    key: 'major',
-  },
-  {
-    title: 'ชั้นปี',
-    dataIndex: 'year',
-    key: 'year',
-  },
-  {
-    title: 'ค่าห้อง',
-    dataIndex: 'roomRate',
-    key: 'year',
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: 'A',
-    name: 'John Brown',
-    sid: 'B6512345',
-    major: 'Engineering',
-    year: 3,
-    roomRate: '2,900',
-  },
-  {
-    key: 'B',
-    name: 'Jim Green',
-    sid: 'B6554321',
-    major: 'Engineering',
-    year: 3,
-    roomRate: '2,900',
-  },
-  {
-    key: 'C',
-    name: 'Joe Black',
-    sid: 'B6543210',
-    major: 'Engineering',
-    year: 3,
-    roomRate: '2,900',
-  },
-];
-
-const Listpages: React.FC = () => <Table columns={columns} dataSource={data} />;
 
 export default Listpages;
