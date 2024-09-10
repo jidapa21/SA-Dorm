9ee99f068726c5433bee54cd8bcb00679abd049a
jidapa21 <ahmpan21@gmail.com>
2024-09-10 19:42:40 +0700

    save

--- backend/entity/expense.go
+++ backend/entity/expense.go
@@ -7,13 +7,13 @@ type Expense struct {
 	Remark string `json:"remark"`
 	Status string `json:"status"`
 
-	RentFeeID uint     `json:"ex_id"`
+	RentFeeID uint     `json:"rent_id"`
 	RentFees   *RentFee `gorm:"foreignKey: RentFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
 
-	ElectricityFeeID uint     `json:"ex_id"`
+	ElectricityFeeID uint     `json:"elec_id"`
 	ElectricityFees   *ElectricityFee `gorm:"foreignKey: ElectricityFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
 
-	WaterFeeID uint     `json:"ex_id"`
+	WaterFeeID uint     `json:"water_id"`
 	WaterFees   *WaterFee `gorm:"foreignKey: WaterFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
 
 	Slips []Slip `gorm:"foreignKey:ExpenseID"`
