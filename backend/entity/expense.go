167404dcdfba870b1ab02c3250b4cc2f63809deb
Jularatpts <Jularatpts@gmail.com>
2024-09-10 19:42:04 +0700

    ice

--- backend/entity/expense.go
+++ backend/entity/expense.go
@@ -7,14 +7,14 @@ type Expense struct {
 	Remark string `json:"remark"`
 	Status string `json:"status"`
 
-	RentFeeID uint     `json:"ex_id"`
-	RentFees   *RentFee `gorm:"foreignKey: RentFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
+	RentFeeID uint     `json:"rent_id"`
+	RentFees   *RentFee `gorm:"foreignKey: RentFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"rentfee"`
 
-	ElectricityFeeID uint     `json:"ex_id"`
-	ElectricityFees   *ElectricityFee `gorm:"foreignKey: ElectricityFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
+	ElectricityFeeID uint     `json:"elec_id"`
+	ElectricityFees   *ElectricityFee `gorm:"foreignKey: ElectricityFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"electricityfee"`
 
-	WaterFeeID uint     `json:"ex_id"`
-	WaterFees   *WaterFee `gorm:"foreignKey: WaterFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"expense"`
+	WaterFeeID uint     `json:"water_id"`
+	WaterFees   *WaterFee `gorm:"foreignKey: WaterFeeID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"waterfee"`
 
 	Slips []Slip `gorm:"foreignKey:ExpenseID"`
 }
